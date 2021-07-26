c6c461315764e8e5ed9c31d9c6d60c1bb620514f
Andrew Austin <2098085+nitsuaa@users.noreply.github.com>
2021-07-26 20:32:23 +0100

    Add drag-to-resize handle to top-left of notes box.
    Keeps size after closing and re-opening box (but not on reload yet).

--- awbw-notes.user.js
+++ awbw-notes.user.js
@@ -1,20 +1,27 @@
 // ==UserScript==
 // @name         AWBW Notepad
 // @namespace    https://github.com/MikeFHay/
-// @version      0.2
+// @version      0.3
 // @description  Adds per-game notes to Advance Wars By Web
 // @author       https://github.com/MikeFHay
 // @match        https://awbw.amarriner.com/2030.php?games_id=*
 // @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
+    $("head").append (
+        '<link href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" '
+      + 'rel="stylesheet" type="text/css">'
+    );
+
     'use strict';
     const key = "notes_" + gameId;
     var text = localStorage.getItem(key) || "";
 
     var textBox = document.createElement("textarea");
+    textBox.setAttribute("id", "notesTextArea");
     textBox.setAttribute("class", "notesClosable");
     textBox.setAttribute("style", "position:inline;width:200px;height:100px;");
     textBox.value = text;
@@ -24,7 +31,7 @@
     closeButton.setAttribute("id", "notesClose");
 
     var divBox = document.createElement("div");
-    divBox.innerHTML = "<header class='notesClosable'>Notes:</header>";
+    divBox.innerHTML = "<span class='notesAbs'>&#8689</span><header class='notesClosable'>Notes:</header>";
     divBox.setAttribute("id", "playerNotesArea");
     divBox.prepend(closeButton);
     divBox.appendChild(textBox);
@@ -40,20 +47,20 @@
         event.stopPropagation();
     });
 
+    $("#notesTextArea").resizable({
+        handles: "nw"
+    });
+    $("#notesTextArea").parent().addClass("notesClosable"); //Make the 'parent' resizing div disappear on click too.
+    $("#notesTextArea").parent()[0].style.position = null; //HACK: Move the resizable() handle to the top-left, ignoring header.
+
     var isOpen = true;
     closeButton.addEventListener("click", function(event) {
         if(isOpen) {
-            var closableThings = document.getElementsByClassName("notesClosable");
-            for (var i = 0; i < closableThings.length; i++) {
-                closableThings[i].style.display = "none";
-            }
+            $(".notesClosable").css("display", "none");
             closeButton.innerHTML = "&#9650;"
             isOpen = false;
         } else {
-            var closableThings = document.getElementsByClassName("notesClosable");
-            for (var i = 0; i < closableThings.length; i++) {
-                closableThings[i].style.display = "block";
-            }
+            $(".notesClosable").css("display", "block");
             closeButton.innerHTML = "&#9660;"
             isOpen = true;
         }
@@ -61,23 +68,34 @@
 
     GM_addStyle(`
         #playerNotesArea {
-           position:fixed;
+           position: fixed;
            bottom: 0px;
            right: 0px;
            border: 1px solid black;
            background-color: white;
+           z-index: 99;
         }
 
         #notesClose {
-            position:absolute;
-            right:0px;
+            position: absolute;
+            right: 0px;
             top: -20px;
             border: 1px solid black;
             background-color: white;
         }
 
+        #notesTextArea {
+            resize: none;
+        }
+
         .notesClosable {
             display: block;
+        }
+        
+        .notesAbs {
+            position: absolute;
+            top: -5px;
+            left: -1px;
         }
     `);
 })();
